93ac346fc8373e296a142cd0408a1a813b43cd3d
Gwendal LABRUE <g.labrue@groupe-creative.fr>
2019-12-09 15:23:54 +0100

    validator - Section 7 finie

--- src/Entity/Invoice.php
+++ src/Entity/Invoice.php
@@ -2,10 +2,34 @@
 
 namespace App\Entity;
 
+use App\Entity\User;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
+
+
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\InvoiceRepository")
+ * @ApiResource(
+ *  subresourceOperations={
+ *      "api_customers_invoices_get_subresource"={
+ *          "normalization_context"={"groups"={"invoices_subresource"}}
+ *  }
+ * },
+ *  attributes={
+ *      "pagination_enabled"=true,
+ *      "pagination_items_per_page"=20,
+ *      "order":{"amount":"desc"}
+ *  },
+ *  normalizationContext={"groups"={"invoices_read"}},
+ *  denormalizationContext={"disable_type_enforcement"=true}
+ * )
+ * @ApiFilter(OrderFilter::class, properties={"amount","sentAt"})
  */
 class Invoice
 {
@@ -13,32 +37,53 @@ class Invoice
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups({"invoices_read", "customers_read","invoices_subresource"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="float")
+     * @Groups({"invoices_read", "customers_read","invoices_subresource"})
+     * @Assert\Type(type="numeric", message="le montant de la facture doit être numerique")
+     * @Assert\NotBlank(message= "Le montant ne peut être vide")
      */
     private $amount;
 
+
     /**
      * @ORM\Column(type="datetime")
+     * @Groups({"invoices_read", "customers_read","invoices_subresource"})
+     * @Assert\NotBlank(message="La date d'envoi doit être renseignée")   
      */
     private $sentAt;
 
+    
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"invoices_read", "customers_read","invoices_subresource"})
+     * @Assert\NotBlank(message="Le statut doit être renseigné")
+     * @Assert\Choice(choices={"PAID", "SENT","CANCELLED"}, message="le statut doit être cancelled, paid ou sent")
+
      */
     private $status;
 
+    
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\Customer", inversedBy="invoices")
      * @ORM\JoinColumn(nullable=false)
+     * @Groups({"invoices_read"})
+     * @Assert\NotBlank(message="Le customer doit être renseigné")
+
      */
     private $customer;
 
+
+
     /**
      * @ORM\Column(type="integer")
+     * @Groups({"invoices_read", "customers_read","invoices_subresource"})
+     * @Assert\NotBlank(message="il faut un chrono")
+     * @Assert\Type(type="integer", message="le chrono doit être un int")
      */
     private $chrono;
 
@@ -46,13 +91,21 @@ class Invoice
     {
         return $this->id;
     }
+    /**
+     * retourne le user
+     * @Groups({"invoices_read","invoices_subresource"})
+     * @return User
+     */
+    public function getUser() : ?User {
+        return $this->customer->getUser();
+    }
 
     public function getAmount(): ?float
     {
         return $this->amount;
     }
 
-    public function setAmount(float $amount): self
+    public function setAmount($amount): self
     {
         $this->amount = $amount;
 
@@ -100,7 +153,7 @@ class Invoice
         return $this->chrono;
     }
 
-    public function setChrono(int $chrono): self
+    public function setChrono($chrono): self
     {
         $this->chrono = $chrono;
 
